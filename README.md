185c02a8d107ecc8f05c5eb6b748fa9ed0d2433e
yqf <1226283409@qq.com>
2021-01-31 17:07:38 +0800

    update

---
+++ README.md
@@ -0,0 +1,37 @@
+# vue-cartoon
+
+## Project setup
+```
+cnpm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run dev
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### 配置说明 static/config.js
+
+```
+配置文件
+
+APPID //百度翻译api需要的APPID
+KEY //百度翻译api需要的KEY
+LOADNUM = 5 //max10 ，min 1,1秒调用的接口数量
+从FROM语言转换为TO语言
+FROM = 'zh';
+TO = 'en';
+```
+
+### 配置说明 static/lang.js
+
+```
+需要翻译的文件
+
+const LANG = {};
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path')
 module.exports = {
+    entry: path.resolve(__dirname, './src/index.js'),
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, './dist')
+    },
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebPackPlugin({
             template: "./index.html",
-            filename: "./index.html"
-        })
+            filename: "./index.html",
+            minify: false,
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: path.resolve(__dirname + '/static'), to: "static", globOptions: { ignore: ['.*'] } }
+            ],
+        }),
     ]
 }
